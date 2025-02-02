cb37d128f3ef04f4508f64c0a24682514196014b
BugHunter-X <askzenai@gmail.com>
2025-02-02 08:50:04 +0100

    Add bug.groovy file

---
+++ bug.groovy
@@ -0,0 +1,10 @@
+```groovy
+class MyClass {
+    def myMethod(arg) {
+        if (arg == null) {
+            return null // This might cause unexpected behavior
+        }
+        // ... rest of the method
+    }
+}
+```
